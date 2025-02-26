7f12dba08334bf8f49604a61fcab91f3734d732f
eddy <eddy.mwaniki.27@gmail.com>
2025-02-26 10:39:57 +0300

    changes magazine

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -77,10 +77,10 @@ class Magazine:
     all = []
     
     def __init__(self, name, category):
-        if not isinstance(name, str) or not (2 <= len(name) <= 16):
-            raise ValueError("Name must be a string between 2 and 16 characters.")
-        if not isinstance(category, str) or len(category) == 0:
-            raise ValueError("Category must be a non-empty string.")
+        # if not isinstance(name, str) or not (2 <= len(name) <= 16):
+        #     raise ValueError("Name must be a string between 2 and 16 characters.")
+        # if not isinstance(category, str) or len(category) == 0:
+        #     raise ValueError("Category must be a non-empty string.")
         
         self._name = name
         self._category = category
@@ -89,22 +89,18 @@ class Magazine:
     @property
     def name(self):
         return self._name
+    @name.setter
+    def name(self, name) :
+        if isinstance(name, str) and (2 <= len(name) <= 16) :
+            self._name = name
     
     @property
     def category(self):
         return self._category
-    
-    @name.setter
-    def name(self, value):
-        if not isinstance(value, str) or not (2 <= len(value) <= 16):
-            raise ValueError("Name must be a string between 2 and 16 characters.")
-        self._name = value
-    
     @category.setter
-    def category(self, value):
-        if not isinstance(value, str) or len(value) == 0:
-            raise ValueError("Category must be a non-empty string.")
-        self._category = value
+    def category(self, category) :
+        if isinstance(category, str) and len(category) > 0:
+            self._category = category
     
     def articles(self):
         return [article for article in Article.all if article.magazine == self]
