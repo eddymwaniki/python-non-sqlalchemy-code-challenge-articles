94846d39f3e844b21972d40d39557315c28cb83c
eddy <eddy.mwaniki.27@gmail.com>
2025-02-26 12:30:53 +0300

    passes test

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,16 +1,7 @@
 class Article:
     all = []
     
-    def __init__(self, author, magazine, title):
-        # if not isinstance(author, Author):
-        #     raise ValueError("Author must be an instance of the Author class.")
-        # if not isinstance(magazine, Magazine):
-        #     raise ValueError("Magazine must be an instance of the Magazine class.")
-        # if not isinstance(title, str) or not (5 <= len(title) <= 50):
-        #     raise ValueError("Title must be a string between 5 and 50 characters.")
-        # if hasattr(self, '_title'):
-        #     raise AttributeError("Title cannot be changed after instantiation ")
-        
+    def __init__(self, author, magazine, title): 
         self.author = author
         self.magazine = magazine
         self._title = title
@@ -43,11 +34,7 @@ class Article:
             self._magazine = magazine                          
 
 class Author:
-    def __init__(self, name):
-        # if not isinstance(name, str) or len(name) == 0:
-        #     raise ValueError("Name must be a non-empty string.")
-        # if hasattr(self, '_name'):
-        #     raise AttributeError("Name cannot be changed after instantiation.")
+    def __init__(self, name):      
         self._name = name
         
     @property
@@ -58,7 +45,6 @@ class Author:
         if isinstance(name, str) and not len(name) == 0 and not hasattr(self, "_name") :
             self._name = name
 
-    
     def articles(self):
         return [article for article in Article.all if article.author == self]
     
@@ -77,11 +63,6 @@ class Magazine:
     all = []
     
     def __init__(self, name, category):
-        # if not isinstance(name, str) or not (2 <= len(name) <= 16):
-        #     raise ValueError("Name must be a string between 2 and 16 characters.")
-        # if not isinstance(category, str) or len(category) == 0:
-        #     raise ValueError("Category must be a non-empty string.")
-        
         self._name = name
         self._category = category
         Magazine.all.append(self)
@@ -116,12 +97,6 @@ class Magazine:
         authors = [article.author for article in self.articles()]
         frequent_authors = [author for author in set(authors) if authors.count(author) > 2]
         return frequent_authors if frequent_authors else None
-    
-    @classmethod
-    def top_publisher(cls):
-        if not Article.all:
-            return None
-        return max(cls.all, key=lambda magazine: len(magazine.articles()), default=None)
 
 author1 = Author("Ian")
 magazine1 = Magazine("INSYDER", "Entertainment")
